0047d9ae58aa2f6816b8ed01a4b5a40d7b3c5457
moubougrine <moughitbougrine@gmail.com>
2024-11-20 17:49:20 +0100

    Delete Projact directory

--- Projact/test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-
-image = cv2.imread('\Projact\\images\\\luux.jpg')
-new = cv2.resize(image,(500,700))
-cv2.imshow('hi',new)
-cv2.imwrite('\Projact\\images\\\luux.jpg',new)
-cv2.waitKey(0)
